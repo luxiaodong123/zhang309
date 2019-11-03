61ba5a4506c8144c5266fa9e1fb55c6478ee900b
luxiaodong <898200262@qq.com>
2019-11-03 11:58:17 +0800

    l 4444444444444

--- yicheng/src/views/agent-login.vue
+++ yicheng/src/views/agent-login.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="u-login-body22222">
     <div>222222222222222222222222222</div>
-    <div>222222222222222222222222222</div>
+    <div>4444444444444444444444444</div>
     <div>222222222222222222222222222</div>
     <div class="u-l-main">
       <div class="u-item">
